55d0e20b72e4db86b056f7b1883ca1b9d49c33d5
Deepak kumar <87894208+Deepak6o@users.noreply.github.com>
2023-04-03 11:08:00 +0530

    Create Climbing Stairs without DP.cpp

---
+++ Climbing Stairs without DP.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n<0)
+        {
+            return 0;
+        }
+        if(n==0)
+        {
+            return 1;
+        }
+        return climbStairs(n-1)+climbStairs(n-2);
+        
+    }
+};
